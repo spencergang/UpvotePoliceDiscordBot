3c6e8f366610278ad1e01df929ff4ba79221280f
gangspencer <s.gang@vikes.csuohio.edu>
2020-02-01 12:01:52 -0500

    Removing config.json

--- bot.js
+++ bot.js
@@ -3,15 +3,26 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const config = require('./config.json')
 
+client.login(auth.token);
+
 client.on('ready', () =>{
 	console.log('Logged in as ' + client.user.tag);
 });
 
 client.on('message', msg => {
-	console.log(msg.member.user);
-	if (msg.content === 'ping'){
-		msg.reply('pong');
+	if (msg.author.username === config.user_to_hate.username) {
+		console.log("user to hate sent message");
+		const filter = (reaction, user) => {
+			return reaction.emoji.name === '👍' && user.username === config.user_to_hate.username;
+		};
+
+		msg.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+			.then(collected => {
+				msg.reply("You dumb bitch " + config.user_to_hate.friendly_name + ", you shouldn't upvote yourself");
+			})
+			.catch(collected => {
+				console.log("Wow, they didn't upvote themselves!");
+			});
 	}
 });
 
-client.login(auth.token);
